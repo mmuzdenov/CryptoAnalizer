4a16c931b22c31f7985620fc34f1af45a610c44a
uzdenov <33064223+uzdenov@users.noreply.github.com>
2025-01-03 14:23:20 +0300

    init commit

---
+++ README.md
@@ -0,0 +1,1 @@
+Uzdenov CryptoAnalyzer project
---
+++ src/main/java/ru/javarush/uzdenov/cryptoanalyzer/Runner.java
@@ -0,0 +1,7 @@
+package ru.javarush.uzdenov.cryptoanalyzer;
+
+public class Runner {
+    public static void main(String[] args) {
+        System.out.println("Hello maven!!!!");
+    }
+}
