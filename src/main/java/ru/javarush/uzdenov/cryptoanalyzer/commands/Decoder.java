a8e51fce7853372d5f3f101b2a6b5a40a403c7c5
uzdenov <33064223+uzdenov@users.noreply.github.com>
2025-01-13 17:54:50 +0300

    Make method makeAlphabet as method class CeasarConvertor. Remove decloration method makeAlphabet from classes Decoder & Encoder.

--- src/main/java/ru/javarush/uzdenov/cryptoanalyzer/commands/Decoder.java
+++ src/main/java/ru/javarush/uzdenov/cryptoanalyzer/commands/Decoder.java
@@ -8,21 +8,13 @@ import static ru.javarush.uzdenov.cryptoanalyzer.constants.Constants.ALPHABET;
 import static ru.javarush.uzdenov.cryptoanalyzer.constants.Constants.TXT_FOLDER;
 
 public class Decoder implements Action{
-    String alphabetFrom;
-    private void makeAlphabet(int key){
-        alphabetFrom= ALPHABET.substring(key) + ALPHABET.substring(0, key);
-        System.out.println("Decode:makeAlphabet");
-        System.out.println(ALPHABET);
-        System.out.println(alphabetFrom);
-    }
 
     @Override
     public Result execute(String[] parameters) {
         Result ret;
-        makeAlphabet(Integer.parseInt(parameters[2]));
         ret = CaesarConvertor.Execute(TXT_FOLDER+parameters[0],
                                 TXT_FOLDER+parameters[1],
-                                 alphabetFrom,
+                                 CaesarConvertor.makeAlphabet(Integer.parseInt(parameters[2])),
                                  ALPHABET);
         return new Result("decode all right", ret.getResultCode());
     }
--- src/main/java/ru/javarush/uzdenov/cryptoanalyzer/commands/Encoder.java
+++ src/main/java/ru/javarush/uzdenov/cryptoanalyzer/commands/Encoder.java
@@ -2,31 +2,19 @@ package ru.javarush.uzdenov.cryptoanalyzer.commands;
 
 import ru.javarush.uzdenov.cryptoanalyzer.commands.tools.CaesarConvertor;
 import ru.javarush.uzdenov.cryptoanalyzer.entity.Result;
-import ru.javarush.uzdenov.cryptoanalyzer.entity.ResultCode;
-
-import java.io.*;
 
 import static ru.javarush.uzdenov.cryptoanalyzer.constants.Constants.ALPHABET;
 import static ru.javarush.uzdenov.cryptoanalyzer.constants.Constants.TXT_FOLDER;
 
 public class Encoder implements Action{
-    private String alphabetTo= "";
-
-    private void makeAlphabet(int key){
-        alphabetTo= ALPHABET.substring(key) + ALPHABET.substring(0, key);
-        System.out.println("Encode:makeAlphabet");
-        System.out.println(ALPHABET);
-        System.out.println(alphabetTo);
-    }
 
     @Override
     public Result execute(String[] parameters) {
-        makeAlphabet(Integer.parseInt(parameters[2]));
         Result ret;
         ret = CaesarConvertor.Execute(TXT_FOLDER+parameters[0],
                 TXT_FOLDER+parameters[1],
                 ALPHABET,
-                alphabetTo);
+                CaesarConvertor.makeAlphabet(Integer.parseInt(parameters[2])));
         return new Result("encode all right", ret.getResultCode());
 
     }
